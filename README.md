004695ae38a6baab483181d67624e37ff8526f1f
Jules Rostand <jules.rostand@outlook.com>
2021-11-10 15:58:45 +0100

    reprise et documentation de la première étape de filtre des DECP sur le périmètre État

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Exploration des DECP et prévision budgétaire
 
 Ce dossier contient le texte et le code de la note étudiant les données essentielles de la commande publique (DECP) au service de l'exercice de prévision budgétaire porté par la direction du Budget.
+
+## Sources et données
+
+Avant de lancer l'exécution des programmes, il convient de disposer des données, selon l’arborescence présentée ci-dessous :
+
+```
+├── data
+│   ├── budget
+│   │   ├── Copie de RestitCalculReferentiel-20211013_13h13m25s-SUPERADM.xls
+│   │   ├── filtresPCE.xlsx
+│   │   └── ZBUD51 budget Etat 2020.csv
+│   ├── decp
+│   │   ├── decp_augmente.csv
+│   │   └── table corr V2.csv
+│   ├── oecp
+│   │   └── oecp_chiffres-clefs-decp-2020_octobre-2021
+```
+
+## Méthodologie et traitement des données
+
+Ce projet nécessite la mise en œuvre d'un étape de sélection manuelle, qui peut être sautée, puis de deux étapes principales.
+
+### Sélection manuelle des données (optionnelle)
+
+La sélection manuelle des données consiste ici à ne sélectionner que les marchés de la commande publique relevant du périmètre de l'État. Pour ce faire, il est nécessaire de disposer d'une liste des acheteurs de l'État. Afin d'établir celle-ci, le script `filtre_budget_etat.py` établit la liste des identifiants et des noms des acheteurs présents dans la base de données. Celle-ci doit être ensuite traitée manuellement, pour spécifier si chaque couple d’identifiant et de nom relève bien de ce périmètre.
---
+++ decp_budget/filtre_perimetre_etat.py
@@ -0,0 +1,47 @@
+# Préparation du filtre sur le périmètre de l'État
+# Ce script a pour fonction de proposer une liste des couples (idAcheteur, nomAcheteur), 
+# à même de nourir une table de sélection des couples relevant du seul périmètre de l'État. 
+
+# Librairies
+import pandas as pd
+
+# Données sources
+decp_safe = pd.read_csv("data/decp_augmente.csv", 
+	sep = ";", 
+	dtype={'idAcheteur': str}, 
+	encoding="UTF-8")
+
+etat_safe = pd.read_csv('data/ZBUD51 budget Etat 2020.csv', 
+	sep = ";", 
+	thousands=" ", 
+	decimal=",", 
+	dtype={'EUR' : float})
+
+# On ne conserve que les données provenant de l'Agence pour l'information financière de l'ÉTat (AIFE)
+decp_aife = decp[decp['source'] == 'data.gouv.fr_aife']
+
+# On ne conserve que les informations (vraiment) essentielles à ce stade, en excluant notamment la géolocalisation, etc.
+keep_cols_work = ['type', 'natureObjetMarche', 'objetMarche',
+		'codeCPV_Original', 'codeCPV', 'codeCPV_division', 'referenceCPV',
+		'dateNotification', 'anneeNotification', 'moisNotification',
+		'datePublicationDonnees', 'dureeMois', 'dureeMoisEstimee',
+		'dureeMoisCalculee', 'montant', 'nombreTitulaireSurMarchePresume',
+		'montantCalcule', 'formePrix', 'nature',
+		'accord-cadrePresume', 'procedure', 'idAcheteur', 'sirenAcheteurValide',
+		'nomAcheteur']
+decp_work = decp_aife[keep_cols_work]
+
+# On ne conserve que les colonnes nécessaires à l'identification de l'acheteur
+keep_cols_acheteur = ['idAcheteur',
+'nomAcheteur', 'libelleRegionAcheteur',
+'libelleDepartementAcheteur',
+'codePostalAcheteur', 'libelleCommuneAcheteur']
+decp_acheteur = decp_aife[keep_cols_acheteur]
+
+# Regroument par identifiant et nom des acheteurs
+res = decp_acheteur.groupby(['idAcheteur', 'nomAcheteur']).all().sort_values('idAcheteur')
+
+# Export en CSV
+res.to_csv("select_acheteurs_grouped.csv", 
+	sep = ";", 
+	encoding="UTF-8")
